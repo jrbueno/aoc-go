c2fc396fe70889a23cfd422e65180141d6dd4f15
Jose Bueno <dev@jrbueno.com>
2024-12-13 17:23:26 -0500

    day2 part 2 not working

--- solutions/year2024/day2/day2.go
+++ solutions/year2024/day2/day2.go
@@ -1,10 +1,10 @@
 package day2
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"math"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -14,12 +14,9 @@ func RunAll(year int, day int, input []string, input2 []string) (partOneResult a
 }
 
 func RunPartOne(input []string, year int, day int) any {
-	//var reports []bool
 	safeCount := 0
-	scanner := bufio.NewScanner(strings.NewReader(strings.Join(input, "\n")))
-	for scanner.Scan() {
+	for _, l := range input {
 		var r []int
-		l := scanner.Text()
 		nums := strings.Fields(l)
 		isSafe := true
 		direction := 0
@@ -51,8 +48,7 @@ func RunPartOne(input []string, year int, day int) any {
 		if isSafe {
 			safeCount++
 		}
-		fmt.Printf("Row: %v Safe:%v\n", r, isSafe)
-		//reports = append(reports, isSafe)
+		//fmt.Printf("Row: %v Safe:%v\n", r, isSafe)
 	}
 
 	fmt.Printf("Total Safe: %d\n", safeCount)
@@ -60,5 +56,61 @@ func RunPartOne(input []string, year int, day int) any {
 }
 
 func RunPartTwo(input []string, year int, day int) any {
-	return nil
+	safeCount := 0
+	for _, l := range input {
+		var r []int
+		reports := strings.Fields(l)
+		var isSafe bool
+		direction := 0
+		for _, num := range reports {
+			n, err := strconv.Atoi(num)
+			if err != nil {
+				log.Fatalf("Error parsing number %s\n%v", num, err)
+			}
+			r = append(r, n)
+		}
+
+		fmt.Printf("Processing Row: %v\n", r)
+		for i := 0; i < len(r); i++ {
+			if i == 0 {
+				continue
+			}
+
+			isSafe = compareLevel(direction, r, i)
+			if !isSafe && i < len(r)-1 {
+				fmt.Printf("UnSafe Row: %v index:%v\n", r, i)
+				blah := slices.Delete(slices.Clone(r), i, i+1)
+				fmt.Printf("Blah Row: %v\n", blah)
+				isSafe = compareLevel(direction, blah, i)
+				fmt.Printf("Blah Row Safe: %v\n", isSafe)
+			}
+
+			if !isSafe {
+				break
+			}
+
+			direction = r[i-1] - r[i]
+		}
+		if isSafe {
+			fmt.Printf("Row: %v Safe:%v\n", r, isSafe)
+			safeCount++
+		}
+	}
+
+	fmt.Printf("Total Safe: %d\n", safeCount)
+	return safeCount
+}
+
+func compareLevel(direction int, r []int, i int) bool {
+	if direction > 0 && r[i-1] < r[i] {
+		return false
+	}
+	if direction < 0 && r[i-1] > r[i] {
+		return false
+	}
+	d := int(math.Abs(float64(r[i-1] - r[i])))
+	if d == 0 || d > 3 {
+		return false
+	}
+	return true
 }
--- solutions/year2024/day2/day2_test.go
+++ solutions/year2024/day2/day2_test.go
@@ -19,4 +19,12 @@ func TestDay2PartOne(t *testing.T) {
 			t.Errorf("got %v want %v", got, want)
 		}
 	})
+	t.Run("PartTwo with test Input", func(t *testing.T) {
+		lines := []string{"7 6 4 2 1", "1 2 7 8 9", "9 7 6 2 1", "1 3 2 4 5", "8 6 4 4 1", "1 3 6 7 9"}
+		got := RunPartTwo(lines, 2024, 2)
+		want := 4
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
