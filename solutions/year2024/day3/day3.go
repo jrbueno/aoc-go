d7ab1dfc15fe9639593a143378898d069a990c39
Jose Bueno <dev@jrbueno.com>
2024-12-14 22:26:22 -0500

    day3 part 2 done

--- solutions/year2024/day3/day3.go
+++ solutions/year2024/day3/day3.go
@@ -13,7 +13,7 @@ func RunPartOne(lines []string, year int, day int) any {
 		re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
 		matches := re.FindAllStringSubmatch(line, -1)
 		for _, match := range matches {
-			fmt.Printf("Match: %v\n", match)
+			//fmt.Printf("Match: %v\n", match)
 			a, err := strconv.Atoi(match[1])
 			if err != nil {
 				fmt.Printf("Error converting %v to int: %v\n", match[1], err)
@@ -36,6 +36,37 @@ func RunAll(year int, day int, input []string, input2 []string) (partOneResult a
 	return RunPartOne(input, year, day), RunPartTwo(input2, year, day)
 }
 
-func RunPartTwo(input []string, year int, day int) any {
-	return 0
+func RunPartTwo(lines []string, year int, day int) any {
+	results := 0
+
+	isEnabled := true
+	for _, line := range lines {
+		mulRegEx := regexp.MustCompile(`mul\((\d+),(\d+)\)|do\(\)|don't\(\)`)
+		mulValues := mulRegEx.FindAllStringSubmatch(line, -1)
+		for _, match := range mulValues {
+			if match[0] == "do()" {
+				isEnabled = true
+				continue
+			} else if match[0] == "don't()" {
+				isEnabled = false
+				continue
+			} else if !isEnabled {
+				continue
+			}
+			//fmt.Printf("Enabled Match: %v\n", match)
+			a, err := strconv.Atoi(match[1])
+			if err != nil {
+				//fmt.Printf("Error converting %v to int: %v\n", match[1], err)
+				continue
+			}
+			b, err := strconv.Atoi(match[2])
+			if err != nil {
+				//fmt.Printf("Error converting %v to int: %v\n", match[2], err)
+				continue
+			}
+			results += a * b
+			//fmt.Printf("a: %v, b: %v\n", a, b)
+		}
+	}
+	return results
 }
--- solutions/year2024/day3/day3_test.go
+++ solutions/year2024/day3/day3_test.go
@@ -13,4 +13,12 @@ func TestDay3(t *testing.T) {
 			t.Errorf("got %v want %v", got, want)
 		}
 	})
+	t.Run("Part 2", func(t *testing.T) {
+		lines := []string{"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"}
+		want := 48
+		got := RunPartTwo(lines, 2024, 3)
+		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
